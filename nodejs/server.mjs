bb1cc70e1ef46307c5823eb5ed99ba61dfa7e741
Alexandre Marques Tortoza Canoa <a.marquestortoza@gmail.com>
2025-12-14 19:14:37 -0400

    update

--- nodejs/server.mjs
+++ nodejs/server.mjs
@@ -1,28 +1,17 @@
 import { createServer } from "node:http";
 
-const frase1 = Promise.resolve("Ola ");
-const frase2 = Promise.resolve("Mundo!");
-const frases = [];
-
-const frasesPromises = [frase1, frase2];
-
-for await (const frase of frasesPromises) {
-  frases.push(frase);
-}
-
-// console.log(frases.join(""));
-
-const part1 = Buffer.from("Ola ");
-const part2 = Buffer.from("Mundo!");
-
-const final = Buffer.concat([part1, part2]);
-console.log(final);
-console.log(final.toString("utf-8"));
-
-const server = createServer((request, response) => {
+const server = createServer(async (request, response) => {
   const url = new URL(request.url || "/", "http://localhost");
 
   response.statusCode == 200;
+
+  const chunks = [];
+
+  for await (const chunk of request) {
+    chunks.push(chunk);
+  }
+
+  console.log(Buffer.concat(chunks).toString("utf-8"));
 
   if (request.method === "GET" && url.pathname == "/") {
     response.statusCode = 200;
@@ -33,9 +22,6 @@ const server = createServer((request, response) => {
     response.end("Page not Found");
     return;
   }
-
-  response.setHeader("Content-Type", "text-html");
-  response.end("Hello World!");
 });
 
 server.listen(3000, () => {
