8e0766f2940a1e2c59e44dba7d67d29b056b6bdf
Alexandre Marques Tortoza Canoa <alexandre.tortoza@proton.me>
2026-02-23 00:16:06 -0300

    vou fazer outro codigo, depois voltamos para esse

---
+++ rust/minigrep/minigrep/target/debug/deps/minigrep-ccdf2ffd4478a802.d
@@ -0,0 +1,5 @@
+/home/alexmrtr/recreational/learning/rust/minigrep/minigrep/target/debug/deps/minigrep-ccdf2ffd4478a802.d: src/main.rs
+
+/home/alexmrtr/recreational/learning/rust/minigrep/minigrep/target/debug/deps/minigrep-ccdf2ffd4478a802: src/main.rs
+
+src/main.rs:
---
+++ rust/minigrep/minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/alexmrtr/recreational/learning/rust/minigrep/minigrep/target/debug/minigrep: /home/alexmrtr/recreational/learning/rust/minigrep/minigrep/src/main.rs
